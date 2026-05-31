239105642f6c099bf027e8fafec3fa8488400870
Polok Sarker <ppcs9998@gmail.com>
2026-05-31 12:28:26 +0600

    Add simple calculator functionality in calculator.py
    This is my first Python project.
    It is a simple calculator that performs basic operations.
    
    Created by: Polok Sarker

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# Simple Calculator
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum =", a + b)
+print("Sub =", a - b)
+print("Mul =", a * b)
+print("Div =", a / b)
